d80d16bd31df1d5f3e4a288b6427966b18fac075
hzqst <113660872@qq.com>
2021-09-16 08:15:53 +0800

    Initial commit

---
+++ fallguys/dllapi.cpp
@@ -0,0 +1,314 @@
+// vi: set ts=4 sw=4 :
+// vim: set tw=75 :
+
+/*
+ * Copyright (c) 2001-2006 Will Day <willday@hpgx.net>
+ *
+ *    This file is part of Metamod.
+ *
+ *    Metamod is free software; you can redistribute it and/or modify it
+ *    under the terms of the GNU General Public License as published by the
+ *    Free Software Foundation; either version 2 of the License, or (at
+ *    your option) any later version.
+ *
+ *    Metamod is distributed in the hope that it will be useful, but
+ *    WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with Metamod; if not, write to the Free Software Foundation,
+ *    Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *    In addition, as a special exception, the author gives permission to
+ *    link the code of this program with the Half-Life Game Engine ("HL
+ *    Engine") and Modified Game Libraries ("MODs") developed by Valve,
+ *    L.L.C ("Valve").  You must obey the GNU General Public License in all
+ *    respects for all of the code used other than the HL Engine and MODs
+ *    from Valve.  If you modify this file, you may extend this exception
+ *    to your version of the file, but you are not obligated to do so.  If
+ *    you do not wish to do so, delete this exception statement from your
+ *    version.
+ *
+ */
+
+#include <extdll.h>
+
+#include <dllapi.h>
+#include <meta_api.h>
+
+#include <detours.h>
+
+trace_t *(*g_pfnSV_PushEntity)(trace_t * trace, edict_t *ent, float * push) = NULL;
+
+void (*g_pfnSV_PushMove)(edict_t *pusher, float movetime) = NULL;
+
+void(*g_pfnSV_PushRotate)(edict_t *pusher, float movetime) = NULL;
+
+static bool g_bIsPushMove = false;
+static bool g_bIsPushRotate = false;
+static edict_t *g_PusherEntity;
+
+void NewSV_PushMove(edict_t *pusher, float movetime)
+{
+	g_bIsPushMove = true;
+	g_PusherEntity = pusher;
+
+	g_pfnSV_PushMove(pusher, movetime);
+
+	g_bIsPushMove = false;
+}
+
+void NewSV_PushRotate(edict_t *pusher, float movetime)
+{
+	g_bIsPushRotate = true;
+	g_PusherEntity = pusher;
+
+	g_pfnSV_PushRotate(pusher, movetime);
+
+	g_bIsPushRotate = false;
+}
+
+static bool g_bIsPushEntity = false;
+static edict_t *g_PushEntity;
+static edict_t *g_PendingEntities[512] = { 0 };
+static int g_NumPendingEntities = 0;
+
+trace_t *NewSV_PushEntity(trace_t * trace, edict_t *ent, float *push)
+{
+	g_bIsPushEntity = true;
+
+	g_PushEntity = ent;
+
+	auto r = g_pfnSV_PushEntity(trace, ent, push);
+
+	//push pending entities...
+	for (int i = 0; i < g_NumPendingEntities; ++i)
+	{
+		auto pPendingEntity = g_PendingEntities[i];
+
+		g_PushEntity = pPendingEntity;
+
+		trace_t temp;
+		g_pfnSV_PushEntity(&temp, pPendingEntity, push);
+	}
+
+	g_NumPendingEntities = 0;
+
+	g_bIsPushEntity = false;
+
+	if ((g_bIsPushMove || g_bIsPushRotate) && g_PusherEntity && g_PusherEntity->v.sequence == 114514)
+	{
+		if ((ent->v.solid == SOLID_SLIDEBOX || ent->v.solid == SOLID_BBOX) && (ent->v.movetype == MOVETYPE_STEP || ent->v.movetype == MOVETYPE_WALK))
+		{
+			vec3_t dir;
+			dir.x = push[0];
+			dir.y = push[1];
+			dir.z = push[2];
+			dir = dir.Normalize();
+
+			ent->v.velocity = dir * g_PusherEntity->v.armorvalue;
+			ent->v.flags |= FL_BASEVELOCITY;
+		}
+	}
+
+	return r;
+}
+
+void NewTouch(edict_t *pentTouched, edict_t *pentOther) {
+
+	if (g_bIsPushEntity && pentTouched == g_PushEntity && g_NumPendingEntities < 512)
+	{
+		if ((pentTouched->v.solid == SOLID_SLIDEBOX || pentTouched->v.solid == SOLID_BBOX) && (pentTouched->v.movetype == MOVETYPE_STEP || pentTouched->v.movetype == MOVETYPE_WALK))
+		{
+			if ((pentOther->v.solid == SOLID_SLIDEBOX || pentOther->v.solid == SOLID_BBOX) && (pentOther->v.movetype == MOVETYPE_STEP || pentOther->v.movetype == MOVETYPE_WALK))
+			{
+				//Don't append same entity twice
+				for (int i = 0; i < g_NumPendingEntities; ++i)
+				{
+					if (g_PendingEntities[i] == pentOther)
+					{
+						SET_META_RESULT(MRES_IGNORED);
+						return;
+					}
+				}
+
+				g_PendingEntities[g_NumPendingEntities] = pentOther;
+				g_NumPendingEntities++;
+
+				SET_META_RESULT(MRES_HANDLED);
+			}
+		}
+	}
+	SET_META_RESULT(MRES_IGNORED);
+}
+
+static DLL_FUNCTIONS gFunctionTable = 
+{
+	NULL,					// pfnGameInit
+	NULL,					// pfnSpawn
+	NULL,					// pfnThink
+	NULL,					// pfnUse
+	NewTouch,					// pfnTouch
+	NULL,					// pfnBlocked
+	NULL,					// pfnKeyValue
+	NULL,					// pfnSave
+	NULL,					// pfnRestore
+	NULL,					// pfnSetAbsBox
+
+	NULL,					// pfnSaveWriteFields
+	NULL,					// pfnSaveReadFields
+
+	NULL,					// pfnSaveGlobalState
+	NULL,					// pfnRestoreGlobalState
+	NULL,					// pfnResetGlobalState
+
+	NULL,					// pfnClientConnect
+	NULL,					// pfnClientDisconnect
+	NULL,					// pfnClientKill
+	NULL,					// pfnClientPutInServer
+	NULL,					// pfnClientCommand
+	NULL,					// pfnClientUserInfoChanged
+	NULL,					// pfnServerActivate
+	NULL,					// pfnServerDeactivate
+
+	NULL,					// pfnPlayerPreThink
+	NULL,					// pfnPlayerPostThink
+
+	NULL,					// pfnStartFrame
+	NULL,					// pfnParmsNewLevel
+	NULL,					// pfnParmsChangeLevel
+
+	NULL,					// pfnGetGameDescription
+	NULL,					// pfnPlayerCustomization
+
+	NULL,					// pfnSpectatorConnect
+	NULL,					// pfnSpectatorDisconnect
+	NULL,					// pfnSpectatorThink
+	
+	NULL,					// pfnSys_Error
+
+	NULL,					// pfnPM_Move
+	NULL,					// pfnPM_Init
+	NULL,					// pfnPM_FindTextureType
+	
+	NULL,					// pfnSetupVisibility
+	NULL,					// pfnUpdateClientData
+	NULL,					// pfnAddToFullPack
+	NULL,					// pfnCreateBaseline
+	NULL,					// pfnRegisterEncoders
+	NULL,					// pfnGetWeaponData
+	NULL,					// pfnCmdStart
+	NULL,					// pfnCmdEnd
+	NULL,					// pfnConnectionlessPacket
+	NULL,					// pfnGetHullBounds
+	NULL,					// pfnCreateInstancedBaselines
+	NULL,					// pfnInconsistentFile
+	NULL,					// pfnAllowLagCompensation
+};
+
+void *MH_SearchPattern(void *pStartSearch, DWORD dwSearchLen, const char *pPattern, DWORD dwPatternLen)
+{
+	PUCHAR dwStartAddr = (PUCHAR)pStartSearch;
+	PUCHAR dwEndAddr = dwStartAddr + dwSearchLen - dwPatternLen;
+
+	while (dwStartAddr < dwEndAddr)
+	{
+		bool found = true;
+
+		for (DWORD i = 0; i < dwPatternLen; i++)
+		{
+			char code = *(char *)(dwStartAddr + i);
+
+			if (pPattern[i] != 0x2A && pPattern[i] != code)
+			{
+				found = false;
+				break;
+			}
+		}
+
+		if (found)
+			return (void *)dwStartAddr;
+
+		dwStartAddr++;
+	}
+
+	return NULL;
+}
+
+DWORD MH_GetModuleSize(HMODULE hModule)
+{
+	return ((IMAGE_NT_HEADERS *)((DWORD)hModule + ((IMAGE_DOS_HEADER *)hModule)->e_lfanew))->OptionalHeader.SizeOfImage;
+}
+
+C_DLLEXPORT int GetEntityAPI2(DLL_FUNCTIONS *pFunctionTable, 
+		int *interfaceVersion)
+{
+	if(!pFunctionTable) {
+		UTIL_LogPrintf("GetEntityAPI2 called with null pFunctionTable");
+		return(FALSE);
+	}
+	else if(*interfaceVersion != INTERFACE_VERSION) {
+		UTIL_LogPrintf("GetEntityAPI2 version mismatch; requested=%d ours=%d", *interfaceVersion, INTERFACE_VERSION);
+		//! Tell metamod what version we had, so it can figure out who is out of date.
+		*interfaceVersion = INTERFACE_VERSION;
+		return(FALSE);
+	}
+	memcpy(pFunctionTable, &gFunctionTable, sizeof(DLL_FUNCTIONS));
+
+	auto engine = GetModuleHandleA("hw.dll");
+
+	if (!engine)
+		engine = GetModuleHandleA("swds.dll");
+
+	if (engine)
+	{
+#define SV_PUSHENTITY_SVENGINE "\x81\xEC\x2A\x2A\x2A\x2A\xA1\x2A\x2A\x2A\x2A\x33\xC4\x89\x44\x24\x2A\x8B\x84\x24\x2A\x00\x00\x00"
+		g_pfnSV_PushEntity = (decltype(g_pfnSV_PushEntity))MH_SearchPattern(engine, MH_GetModuleSize(engine), SV_PUSHENTITY_SVENGINE, sizeof(SV_PUSHENTITY_SVENGINE) - 1);
+
+		if (g_pfnSV_PushEntity)
+		{
+			DetourTransactionBegin();
+			DetourAttach(&(void *&)g_pfnSV_PushEntity, NewSV_PushEntity);
+			DetourTransactionCommit();
+		}
+		else
+		{
+			UTIL_LogPrintf("Failed to locate SV_PushEntity");
+		}
+
+#define SV_PUSHMOVE_SVENGINE "\x81\xEC\x2A\x2A\x2A\x2A\xA1\x2A\x2A\x2A\x2A\x33\xC4\x89\x44\x24\x2A\x2A\x8B\xBC\x24\x88\x00\x00\x00\xD9"
+		g_pfnSV_PushMove = (decltype(g_pfnSV_PushMove))MH_SearchPattern(engine, MH_GetModuleSize(engine), SV_PUSHMOVE_SVENGINE, sizeof(SV_PUSHMOVE_SVENGINE) - 1);
+
+		if (g_pfnSV_PushMove)
+		{
+			DetourTransactionBegin();
+			DetourAttach(&(void *&)g_pfnSV_PushMove, NewSV_PushMove);
+			DetourTransactionCommit();
+		}
+		else
+		{
+			UTIL_LogPrintf("Failed to locate SV_PushMove");
+		}
+
+#define SV_PUSHROTATE_SVENGINE "\x81\xEC\x2A\x2A\x2A\x2A\xA1\x2A\x2A\x2A\x2A\x33\xC4\x89\x84\x24\x2A\x00\x00\x00\x2A\x8B\xBC\x24\xC0\x00\x00\x00\xD9"
+		g_pfnSV_PushRotate = (decltype(g_pfnSV_PushMove))MH_SearchPattern(engine, MH_GetModuleSize(engine), SV_PUSHROTATE_SVENGINE, sizeof(SV_PUSHROTATE_SVENGINE) - 1);
+
+		if (g_pfnSV_PushRotate)
+		{
+			DetourTransactionBegin();
+			DetourAttach(&(void *&)g_pfnSV_PushRotate, NewSV_PushRotate);
+			DetourTransactionCommit();
+		}
+		else
+		{
+			UTIL_LogPrintf("Failed to locate SV_PushMove");
+		}
+	}
+	else
+	{
+		UTIL_LogPrintf("Failed to locate engine dll");
+	}
+
+	return(TRUE);
+}
---
+++ metamod/games.h
@@ -0,0 +1,140 @@
+// vi: set ts=4 sw=4 :
+// vim: set tw=75 :
+ 
+// games.h - list of supported game mods and their data
+ 
+/*
+ * Copyright (c) 2001-2013 Will Day <willday@hpgx.net>
+ *
+ *    This file is part of Metamod.
+ *
+ *    Metamod is free software; you can redistribute it and/or modify it
+ *    under the terms of the GNU General Public License as published by the
+ *    Free Software Foundation; either version 2 of the License, or (at
+ *    your option) any later version.
+ *
+ *    Metamod is distributed in the hope that it will be useful, but
+ *    WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with Metamod; if not, write to the Free Software Foundation,
+ *    Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *    In addition, as a special exception, the author gives permission to
+ *    link the code of this program with the Half-Life Game Engine ("HL
+ *    Engine") and Modified Game Libraries ("MODs") developed by Valve,
+ *    L.L.C ("Valve").  You must obey the GNU General Public License in all
+ *    respects for all of the code used other than the HL Engine and MODs
+ *    from Valve.  If you modify this file, you may extend this exception
+ *    to your version of the file, but you are not obligated to do so.  If
+ *    you do not wish to do so, delete this exception statement from your
+ *    version.
+ *
+ */
+ 
+// This list is now kept in a separate file to facilitate generating the
+// list from game data stored in a convenient db.
+
+#if defined(__x86_64__) || defined(__amd64__)
+#  define MODARCH "_amd64"
+#else
+#  define MODARCH "_i386"
+#endif
+
+	{"action",            "ahl_i386.so",          "ahl.dll",           "Action Half-Life"},
+	{"ag",                "ag_i386.so",           "ag.dll",            "Adrenaline Gamer Steam"},
+	{"ag3",               "hl_i386.so",           "hl.dll",            "Adrenalinegamer 3.x"},
+	{"aghl",              "ag_i386.so",           "ag.dll",            "Adrenalinegamer 4.x"},
+	{"arg",               "arg_i386.so",          "hl.dll",            "Arg!"},
+	{"asheep",            "hl_i386.so",           "hl.dll",            "Azure Sheep"},
+	{"hcfrenzy",          "hcfrenzy.so",              "hcfrenzy.dll",      "Headcrab Frenzy" },
+	{"bdef",              "../cl_dlls/server.so",     "../cl_dlls/server.dll", "Base Defense [Modification]" },
+	{"bdef",              "server.so",                     "server.dll",        "Base Defense [Steam Version]" },
+	{"bg",                "bg_i386.so",           "bg.dll",            "The Battle Grounds"},
+	{"bhl",               "none",                     "bhl.dll",           "Brutal Half-Life" },
+	{"bot",               "bot_i386.so",          "bot.dll",           "Bot"},
+	{"brainbread",        "bb_i386.so",           "bb.dll",            "BrainBread"},
+	{"bumpercars",        "hl_i386.so",           "hl.dll",            "Bumper Cars"},
+	{"buzzybots",         "bb_i386.so",           "bb.dll",            "BuzzyBots"},
+	{"ckf3",              "none",                     "mp.dll",            "Chicken Fortress 3" },
+	{"cs13",              "cs_i386.so",           "mp.dll",            "Counter-Strike 1.3"},
+	{"cstrike",           "cs_i386.so",           "mp.dll",            "Counter-Strike"},
+	{"csv15",             "cs_i386.so",           "mp.dll",            "CS 1.5 for Steam"},
+	{"czero",             "cs_i386.so",           "mp.dll",            "Counter-Strike:Condition Zero"},
+	{"dcrisis",           "dc_i386.so",           "dc.dll",            "Desert Crisis"},
+	{"dmc",               "dmc_i386.so",          "dmc.dll",           "Deathmatch Classic"},
+	{"dod",               "dod_i386.so",          "dod.dll",           "Day of Defeat"},
+	{"dpb",               "pb.i386.so",               "pb.dll",            "Digital Paintball"},
+	{"dragonmodz",        "hl_i386.so",           "mp.dll",            "Dragon Mod Z"},
+	{"esf",               "hl_i386.so",           "hl.dll",            "Earth's Special Forces"},
+	{"existence",         "ex_i386.so",           "existence.dll",     "Existence"},
+	{"firearms",          "fa_i386.so",           "firearms.dll",      "Firearms"},
+	{"firearms25",        "fa_i386.so",           "firearms.dll",      "Retro Firearms"},
+	{"freeze",            "mp_i386.so",           "mp.dll",            "Freeze"},
+	{"frontline",         "front_i386.so",        "frontline.dll",     "Frontline Force"},
+	{"gangstawars",       "gangsta_i386.so",      "gwars27.dll",       "Gangsta Wars"},
+	{"gangwars",          "mp_i386.so",           "mp.dll",            "Gangwars"},
+	{"gearbox",           "opfor_i386.so",        "opfor.dll",         "Opposing Force"},
+	{"globalwarfare",     "gw_i386.so",           "mp.dll",            "Global Warfare"},
+	{"goldeneye",         "golden_i386.so",       "mp.dll",            "Goldeneye"},
+	{"hl15we",            "hl_i386.so",           "hl.dll",            "Half-Life 1.5: Weapon Edition"},
+	{"HLAinGOLDSrc",      "none",                     "hl.dll",            "Half-Life Alpha in GOLDSrc"},
+	{"hlrally",           "hlr_i386.so",          "hlrally.dll",       "HL-Rally"},
+	{"holywars",          "hl_i386.so",           "holywars.dll",      "Holy Wars"},
+	{"hostileintent",     "hl_i386.so",           "hl.dll",            "Hostile Intent"},
+	{"ios",               "ios_i386.so",          "ios.dll",           "International Online Soccer"},
+	{"judgedm",           "judge_i386.so",        "mp.dll",            "Judgement"},
+	{"kanonball",         "hl_i386.so",           "kanonball.dll",     "Kanonball"},
+	{"monkeystrike",      "ms_i386.so",           "monkey.dll",        "Monkeystrike"},
+	{"MorbidPR",          "morbid_i386.so",       "morbid.dll",        "Morbid Inclination"},
+	{"movein",            "hl_i386.so",           "hl.dll",            "Move In!"},
+	{"msc",               "none",                     "ms.dll",            "Master Sword Continued" },
+	{"ns",                "ns_i386.so",           "ns.dll",            "Natural Selection"},
+	{"nsp",               "ns_i386.so",           "ns.dll",            "Natural Selection Beta"},
+	{"oel",               "hl_i386.so",           "hl.dll",            "OeL Half-Life"},
+	{"og",                "og_i386.so",           "og.dll",            "Over Ground"},
+	{"ol",                "ol_i386.so",           "hl.dll",            "Outlawsmod"},
+	{"ops1942",           "spirit_i386.so",       "spirit.dll",        "Operations 1942"},
+	{"osjb",              "osjb_i386.so",         "jail.dll",          "Open-Source Jailbreak"},
+	{"outbreak",          "none",                     "hl.dll",            "Out Break"},
+	{"oz",                "mp_i386.so",           "mp.dll",            "Oz Deathmatch"},
+	{"paintball",         "pb_i386.so",           "mp.dll",            "Paintball"},
+	{"penemy",            "pe_i386.so",           "pe.dll",            "Public Enemy"},
+	{"phineas",           "phineas_i386.so",      "phineas.dll",       "Phineas Bot"},
+	{"ponreturn",         "ponr_i386.so",         "mp.dll",            "Point of No Return"},
+	{"pvk",               "hl_i386.so",           "hl.dll",            "Pirates, Vikings and Knights"},
+	{"rc2",               "rc2_i386.so",          "rc2.dll",           "Rocket Crowbar 2"},
+	{"recbb2",            "recb_i386.so",         "recb.dll",          "Resident Evil : Cold Blood" },
+	{"retrocs",           "rcs_i386.so",          "rcs.dll",           "Retro Counter-Strike"},
+	{"rewolf",            "hl_i386.so",           "gunman.dll",        "Gunman Chronicles"},
+	{"ricochet",          "ricochet_i386.so",     "mp.dll",            "Ricochet"},
+	{"rockcrowbar",       "rc_i386.so",           "rc.dll",            "Rocket Crowbar"},
+	{"rspecies",          "hl_i386.so",           "hl.dll",            "Rival Species"},
+	{"scihunt",           "shunt.so",                 "shunt.dll",         "Scientist Hunt"},
+	{"sdm",               "sdmmod_i386.so",       "sdmmod.dll",        "Special Death Match"},
+	{"Ship",              "ship_i386.so",         "ship.dll",          "The Ship"},
+	{"si",                "si_i386.so",           "si.dll",            "Science & Industry"},
+	{"snow",              "snow_i386.so",         "snow.dll",          "Snow-War"},
+	{"stargatetc",        "hl_i386.so",           "hl.dll",            "StargateTC"},
+	{"svencoop",          "hl_i386.so",           "hl.dll",            "Sven Coop [Modification]" },
+	{"svencoop",          "server.so",                "server.dll",        "Sven Coop [Steam Version]" },
+	{"swarm",             "swarm_i386.so",        "swarm.dll",         "Swarm"},
+	{"tfc",               "tfc_i386.so",          "tfc.dll",           "Team Fortress Classic"},
+	{"thewastes",         "thewastes_i386.so",    "thewastes.dll",     "The Wastes"},
+	{"timeless",          "pt_i386.so",           "timeless.dll",      "Project Timeless"},
+	{"tod",               "hl_i386.so",           "hl.dll",            "Tour of Duty"},
+	{"trainhunters",      "th_i386.so",           "th.dll",            "Train Hunters"},
+	{"trevenge",          "trevenge.so",              "trevenge.dll",      "The Terrorist Revenge"},
+	{"TS",                "ts_i386.so",           "mp.dll",            "The Specialists"},
+	{"tt",                "tt_i386.so",           "tt.dll",            "The Trenches"},
+	{"underworld",        "uw_i386.so",           "uw.dll",            "Underworld Bloodline"},
+	{"valve",             "hl_i386.so",           "hl.dll",            "Half-Life Deathmatch"},
+	{"vs",                "vs_i386.so",           "mp.dll",            "VampireSlayer"},
+	{"wantedhl",          "hl_i386.so",           "wanted.dll",        "Wanted!"},
+	{"wasteland",         "whl_linux.so",             "mp.dll",            "Wasteland"},
+	{"weapon_wars",       "ww_i386.so",           "hl.dll",            "Weapon Wars"},
+	{"wizwars",           "mp_i386.so",           "hl.dll",            "Wizard Wars"},
+	{"wormshl",           "wormshl_i586.so",          "wormshl.dll",       "WormsHL"},
+	{"zp",                "none",                     "mp.dll",            "Zombie Panic"},
