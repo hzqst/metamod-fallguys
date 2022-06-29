0432ce7ed303a5a5dd5e09ec2266bf56041236d4
hzqst <113660872@qq.com>
2022-06-29 14:40:31 +0800

    1

---
+++ fallguys/mhutil.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void *MH_GetModuleBase(const char *name);
+
+size_t MH_GetModuleSize(void *hModule);
+
+void *MH_SearchPattern(void *pStartSearch, size_t dwSearchLen, const char *pPattern, size_t dwPatternLen);
