6671e6d95cbef4a036afa3a70abc67bebbe9a8c9
hzqst <113660872@qq.com>
2023-12-04 00:28:25 +0800

    Add missing file.

---
+++ fallguys/ehandle.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <extdll.h>			// always
+#include <meta_api.h>		// of course
+#include <edict.h>
+#include <const.h>
+#include <enginecallback.h>
+
+class CBaseEntity;
+
+class EHANDLE
+{
+private:
+	edict_t* m_pent;
+	int		m_serialnumber;
+public:
+	EHANDLE()
+	{
+		m_pent = nullptr;
+		m_serialnumber = 0;
+	}
+
+	EHANDLE(edict_t * pEnt)
+	{
+		if (pEnt)
+		{
+			m_pent = pEnt;
+			if (m_pent)
+				m_serialnumber = m_pent->serialnumber;
+		}
+		else
+		{
+			m_pent = NULL;
+			m_serialnumber = 0;
+		}
+	}
+
+	edict_t* Get() const
+	{
+		if (m_pent)
+		{
+			if (m_pent->serialnumber == m_serialnumber)
+				return m_pent;
+			else
+				return nullptr;
+		}
+		return nullptr;
+	}
+	edict_t* Set(edict_t* pent)
+	{
+		m_pent = pent;
+		if (pent)
+			m_serialnumber = m_pent->serialnumber;
+		return pent;
+	}
+
+	operator bool() const
+	{
+		return Get() != NULL;
+	}
+
+	operator edict_t *() const
+	{
+		return Get();
+	}
+
+	edict_t* operator = (edict_t* pEnt)
+	{
+		if (pEnt)
+		{
+			m_pent = pEnt;
+			if (m_pent)
+				m_serialnumber = m_pent->serialnumber;
+		}
+		else
+		{
+			m_pent = NULL;
+			m_serialnumber = 0;
+		}
+		return pEnt;
+	}
+	edict_t* operator ->() const
+	{
+		return Get();
+	}
+};
