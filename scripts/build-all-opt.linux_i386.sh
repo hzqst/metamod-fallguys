4e7c716b3694abd1b2aa691a5a67c0e792051d02
hzqst <113660872@qq.com>
2025-06-07 15:42:42 +0800

    111

---
+++ scripts/build-all-opt.linux_i386.sh
@@ -0,0 +1,4 @@
+sh build-procmap-opt.linux_i386.sh
+sh build-bullet3-opt.linux_i386.sh
+sh build-capstone-opt.linux_i386.sh
+sh build-metamod-cmake-opt.linux_i386.sh
---
+++ scripts/build-all-x86-Release.bat
@@ -0,0 +1,3 @@
+call build-bullet3-x86-Release.bat
+call build-capstone-x86-Release.bat
+call build-metamod-cmake-x86-Release.bat
