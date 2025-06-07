e064ac8ed014701fda4700a1000217a268316dea
hzqst <113660872@qq.com>
2025-06-07 11:16:05 +0800

    update build script

--- scripts/build-metamod.bat
+++ scripts/build-metamod.bat
@@ -1,5 +1,7 @@
 @echo off
 
+setlocal
+
 :: Check if SolutionDir is already set and non-empty
 if not defined SolutionDir (
     :: Only set SolutionDir if it's not already set
@@ -26,3 +28,5 @@ if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
     MSBuild.exe metamod.sln "/target:ascurl" /p:Configuration="Release" /p:Platform="Win32"
     MSBuild.exe metamod.sln "/target:fallguys" /p:Configuration="Release" /p:Platform="Win32"
 )
+
+endlocal
