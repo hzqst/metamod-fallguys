5129c978df0c7a37345056e7b38808848ea35bf7
hzqst <113660872@qq.com>
2026-02-07 23:31:48 +0800

    Add debug scripts.

---
+++ scripts/debug-SvenCoop.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set LauncherExe=svencoop.exe
+set LauncherMod=svencoop
+set FullGameName=Sven Co-op
+set GameAppId=225840
+
+call "debug-helper-AIO.bat"
---
+++ scripts/debug-helper-AIO.bat
@@ -0,0 +1,96 @@
+@echo off
+
+:: Check if SolutionDir is already set and non-empty
+if not defined SolutionDir (
+    :: Only set SolutionDir if it's not already set
+    SET "SolutionDir=%~dp0.."
+)
+
+:: Ensure the path ends with a backslash
+if not "%SolutionDir:~-1%"=="\" SET "SolutionDir=%SolutionDir%\"
+
+if not "%GameDir%"=="" (
+
+    if not exist "%GameDir%\" (
+        echo Error: The GameDir "%GameDir%" is not existing !!!
+        pause
+        exit
+    )
+
+    if not exist "%GameDir%\%LauncherMod%" (
+        echo Error: The ModDir "%GameDir%\%LauncherMod%" is not existing !!!
+        pause
+        exit
+    )
+
+    if not exist "%GameDir%\%LauncherMod%\liblist.gam" (
+        echo Error: The ModDir "%GameDir%\%LauncherMod%" is not a valid Mod !!!
+        pause
+        exit
+    )
+
+    goto start_copy
+)
+
+echo %GameAppId% > "%SolutionDir%tools\steam_appid.txt"
+
+for /f "delims=" %%a in ('"%SolutionDir%\tools\SteamAppsLocation" %GameAppId%') do set OutputString=%%a
+
+if not "%OutputString%"=="" (
+    set "GameDir=%OutputString%"
+    goto start_copy
+)
+
+echo Error: Failed to locate GameInstallDir of %FullGameName%, please make sure Steam is running and you have %FullGameName% installed correctly.
+pause
+exit
+
+:start_copy
+
+echo -----------------------------------------------------
+echo Writing MSVC Properties...
+
+cd /d "%SolutionDir%tools"
+
+copy global_template.props global.props /y
+
+call powershell -Command "(gc global.props) -replace '<MetamodLaunchName>.*</MetamodLaunchName>', '<MetamodLaunchName>%LauncherExe%</MetamodLaunchName>' | Out-File global.props"
+call powershell -Command "(gc global.props) -replace '<MetamodLaunchCommnand>.*</MetamodLaunchCommnand>', '<MetamodLaunchCommnand>-game %LauncherMod%</MetamodLaunchCommnand>' | Out-File global.props"
+call powershell -Command "(gc global.props) -replace '<MetamodGameDirectory>.*</MetamodGameDirectory>', '<MetamodGameDirectory>%GameDir%\</MetamodGameDirectory>' | Out-File global.props"
+call powershell -Command "(gc global.props) -replace '<MetamodModName>.*</MetamodModName>', '<MetamodModName>%LauncherMod%</MetamodModName>' | Out-File global.props"
+
+echo -----------------------------------------------------
+echo Done
+
+@echo off
+
+cd /d "%SolutionDir%"
+
+tasklist | find /i "devenv.exe"
+
+if "%errorlevel%"=="1" (goto ok1) else (goto ok2)
+
+:ok1
+
+for /f "usebackq tokens=*" %%i in (`tools\vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property productPath`) do (
+  set productPath=%%i
+)
+
+if exist "%productPath%" (
+    "%productPath%" metamod.sln
+    exit
+)
+
+@echo You can open metamod.sln with Visual Studio IDE now
+pause
+exit
+
+:ok2
+@echo Please restart Visual Studio IDE to apply changes to the msvc properties
+pause
+exit
+
+:fail
+@echo Failed to locate GameInstallDir of %FullGameName%, please make sure Steam is running and you have %FullGameName% installed correctly.
+pause
+exit
